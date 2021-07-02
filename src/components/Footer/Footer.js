ade061e4b0af1cd4da86d9b231f7dd5a200bdccd
Douglas Obara <dobara@kabarak.ac.ke>
2021-07-02 09:10:02 +0300

    Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import resumeData from "../../utils/resumeData";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>This is the footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <Typography className="footer_name">{resumeData.name}</Typography>
+      </div>
+      <div className="footer_right">
+        <Typography className="footer_copyright">
+          Designed and Developed by{" "}
+          <a
+            href="https://github.com/douglas254"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Douglas254
+          </a>
+          .
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
