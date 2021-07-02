fa57f6398b1ab43a59b0d116c5bfe893dadbd2ce
Douglas Obara <dobara@kabarak.ac.ke>
2021-07-02 08:25:09 +0300

    Header component

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
         <Grid item xs={12} sm={12} md={4} lg={3}>
           <Profile />
         </Grid>
-        <Grid item xs style={{ backgroundColor: "red" }}>
-          <Header />
+        <Grid item xs>
           <Router>
+            <Header />
             <Switch>
               <Route path="/portfolio">
                 <Portfolio />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,81 @@
 import React from "react";
+import {
+  Button,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  HomeRounded,
+  SchoolRounded,
+  WorkRounded,
+  Facebook,
+  Twitter,
+  LinkedIn,
+  GitHub,
+  Telegram,
+} from "@material-ui/icons";
+import { Link, NavLink, withRouter } from "react-router-dom";
+import resumeData from "../../utils/resumeData";
+import CustomButton from "../Button/Button";
+import "./Header.css";
 
-const Header = () => {
-  return <div> This is a header component</div>;
+const Header = (props) => {
+  const pathName = props?.location?.pathname;
+  return (
+    <Navbar expand="lg" sticky="top" className="header">
+      {/* Home link */}
+      <Nav.Link as={NavLink} to="/" className="header_navlink">
+        <Navbar.Brand className="header_home">
+          <HomeRounded />
+        </Navbar.Brand>
+      </Nav.Link>
+
+      <Navbar.Toggle />
+
+      <Navbar.Collapse>
+        <Nav className="header_left">
+          {/* Resume Link */}
+          <Nav.Link
+            as={NavLink}
+            to="/"
+            className={pathName === "/" ? "header_link_active" : "header_link"}
+          >
+            Resume
+          </Nav.Link>
+
+          {/* Portfolio Link */}
+          <Nav.Link
+            as={NavLink}
+            to="/portfolio"
+            className={
+              pathName === "/portfolio" ? "header_link_active" : "header_link"
+            }
+          >
+            Portfolio
+          </Nav.Link>
+        </Nav>
+
+        <div className="header_right">
+          {/* mappinf through the object  */}
+          {Object.keys(resumeData.socials).map((key) => (
+            <a
+              href={resumeData.socials[key].link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {resumeData.socials[key].icon}
+            </a>
+          ))}
+
+          <CustomButton text={"Hire Me"} icon={<Telegram />} />
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
-export default Header;
+export default withRouter(Header);
