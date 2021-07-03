d418300b2782f16cf01435eca712fea076d879ae
Douglas Obara <dobara@kabarak.ac.ke>
2021-07-03 05:23:09 +0300

    resume page(timeline)

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,8 +1,15 @@
 import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import resumeData from "../../utils/resumeData";
+import CustomTimeline, {
+  CustomTimelineSeparator,
+} from "../../components/Timeline/Timeline";
+import WorkIcon from "@material-ui/icons/Work";
+import SchoolIcon from "@material-ui/icons/School";
 
 import "./Resume.css";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import { TimelineContent } from "@material-ui/lab";
 
 const Resume = () => {
   return (
@@ -11,9 +18,7 @@ const Resume = () => {
       <Grid container className="section pb_45">
         <Grid item className="section_title mb_30">
           <span></span>
-          <h6 variant={"h6"} className="section_title_text">
-            About Me
-          </h6>
+          <h6 className="section_title_text">About Me</h6>
         </Grid>
         <Grid item xs={12}>
           <Typography variant={"body2"} className="aboutme_text">
@@ -23,7 +28,68 @@ const Resume = () => {
       </Grid>
 
       {/* Education and Experience  */}
-      <Grid container className="section"></Grid>
+      <Grid container className="section">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6 className="section_title_text">Resume</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container className="resume_timeline">
+            {/* Experiences */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Work Experience" icon={<WorkIcon />}>
+                {/* mapping through the array */}
+                {resumeData.experiences.map((experience) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className="timeline_content">
+                      <Typography className="timeline_title">
+                        {experience.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {experience.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="timeline_description"
+                      >
+                        {experience.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+
+            {/* Education */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Education" icon={<SchoolIcon />}>
+                {/* mapping through the array */}
+                {resumeData.educations.map((education) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className="timeline_content">
+                      <Typography className="timeline_title">
+                        {education.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {education.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="timeline_description"
+                      >
+                        {education.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
 
       {/* Services  */}
       <Grid container className="section"></Grid>
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -37,6 +37,48 @@ const resumeData = {
 
   about:
     "Started earnest brother believe an exposed so. Me he believing daughters if forfeited at furniture. Age again and stuff downs spoke. Late hour new nay able fat each sell. Nor themselves age introduced frequently use unsatiable devonshire get. They why quit gay cold rose deal park. One same they four did ask busy. Reserved opinions fat him nay position. Breakfast as zealously incommode do agreeable furniture. One too nay led fanny allow plate. \n\nQuick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may.",
+
+  experiences: [
+    {
+      title: "Work 1",
+      date: "2018 - Present",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "Work 2",
+      date: "Aug 2016 - Sep 2018",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "Work 3",
+      date: "2014 - 2015",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+  ],
+
+  educations: [
+    {
+      title: "Education 1",
+      date: "2018 - Present",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "Education 2",
+      date: "Aug 2016 - Sep 2018",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "Education 3",
+      date: "2014 - 2015",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+  ],
 };
 
 export default resumeData;
