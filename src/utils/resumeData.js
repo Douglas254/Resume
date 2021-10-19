d824c89ceb7b4cebc1d07bbee4f7fda0706952a5
Douglas254 <dobara@kabarak.ac.ke>
2021-10-19 09:07:12 +0300

    update resumeData

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -15,11 +15,11 @@ const resumeData = {
 
   birthday: "30th July 1997",
   email: "douglasobara97@gmail.com",
-  address: "34567, Nairobi 00100.",
-  phone: "+2547 42 608 717",
+  address: "8303 - 00200 , Kibera Drive.",
+  phone: "(+254)7 42 608 717",
 
   about:
-    "Started earnest brother believe an exposed so. Me he believing daughters if forfeited at furniture. Age again and stuff downs spoke. Late hour new nay able fat each sell. Nor themselves age introduced frequently use unsatiable devonshire get. They why quit gay cold rose deal park. One same they four did ask busy. Reserved opinions fat him nay position. Breakfast as zealously incommode do agreeable furniture. One too nay led fanny allow plate. \n\nQuick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may.",
+    "Hello!, My name is Douglas Obara student at Kabarak University hounding Bachelor of Science in Information Technology(BSc IT),this where i was introduced to the Web Application Programming and since then i developed interest in building web application projects and work with some machine learning models algorithms when i was studying DataScience.I am a Front-end Web Developer.I just fell in love with ReactJs as my favourite library/framework for front-end technology.\n\nCurrently i am learning Java Platform, Enterprise Edition (Java EE) which i found it useful and good to know when i was introduced to HealthIT Systems since it is the technology stack used in the KenyaEMR system, which is based on OpenMRS and many more systems by KabarakHealthIT student innovation hub(iHub) mentors for us to be able to deploy the system to various facilities at County level and even customize to meet the requirements of the facility that the system is being deployed.I always believe in myself in all that i do since am a goal-driven mindset person and passionate for learning new technology that arises in order to solve problems and drive the organization towards achieving its goals, objective ~ effectively & efficiently.I like reading, coding, research, hiking, listening to Music and dancing to the tune.",
 
   socials: {
     Facebook: {
@@ -46,43 +46,31 @@ const resumeData = {
 
   experiences: [
     {
-      title: "Work 1",
-      date: "2018 - Present",
+      title: "Technical Support @WOFAK",
+      date: "Jan 2020 - Dec 2020",
       description:
-        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
-    },
-    {
-      title: "Work 2",
-      date: "Aug 2016 - Sep 2018",
-      description:
-        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
-    },
-    {
-      title: "Work 3",
-      date: "2014 - 2015",
-      description:
-        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+        "Assisted the staff on the technical aspect of the operation in their machines and made sure the system is running as well and troubleshoot the network incase of failure.",
     },
   ],
 
   educations: [
     {
-      title: "Education 1",
-      date: "2018 - Present",
+      title: "KABARAK UNIVERSITY",
+      date: "May 2018 - Present",
       description:
-        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+        "Bachelor of Science in Information Technology (BSc IT)",
     },
     {
-      title: "Education 2",
-      date: "Aug 2016 - Sep 2018",
+      title: "ST. JOHN'S NYAMAGWA BOYS HIGH SCHOOL",
+      date: "2014 - 2017",
       description:
-        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+        "The experience was cool after passing my exam in class eight(KCPE) where i got scholarship by the carolina for kibera (CFK) foundation to pay my secondary school fees signed in the contract which lasted for only four years under certain conditions",
     },
     {
-      title: "Education 3",
-      date: "2014 - 2015",
+      title: "SOWETO ACADEMY",
+      date: "2006 - 2013",
       description:
-        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+        "This where my journey of schooling began and did my best in the kenya certificate of primary education (KCPE)",
     },
   ],
 
@@ -111,22 +99,21 @@ const resumeData = {
       description: [
         "ReactJS",
         "Javascript",
-        "TypeScript",
         "Bootstrap",
         "Material UI",
       ],
     },
     {
       title: "BACK-END",
-      description: ["NodeJS", "Java", "Python", "Solidity"],
+      description: ["Java", "Python","NodeJS"],
     },
     {
       title: "DATABASES",
-      description: ["Firebases", "No SQL Server", "MySQL", "MongoDB"],
+      description: ["MySQL","PostgreSQL", "MongoDB","Firebase"],
     },
     {
       title: "SOURCE CONTROL",
-      description: ["Git", "GitHub", "SCRUM/Agile"],
+      description: ["Git", "GitHub"],
     },
   ],
 
